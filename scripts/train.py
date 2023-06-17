0e9be528e8f550257958cc892d63c6331bd1f3d9
Mohammad Hossein Ahmadi <mh.ahmadi258@gmail.com>
2023-06-18 01:10:48 +0330

    Handle possible bug of paths

--- scripts/train.py
+++ scripts/train.py
@@ -6,8 +6,8 @@ import json
 import sys
 import pprint
 
-sys.path.append(".")
-sys.path.append("..")
+sys.path.insert(0,".")
+sys.path.insert(0,"..")
 
 from options.train_options import TrainOptions
 from training.coach import Coach
